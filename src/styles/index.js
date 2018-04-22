dcb050de7c836acec8ee1c203f48fda223f5c719
Robert Greene <robert@zardozcs.com>
2018-04-22 02:10:30 -0700

    Remove node modules

---
+++ src/styles/index.js
@@ -0,0 +1,13 @@
+// TODO WE NEED TO PARSE ALL CSS FILES
+
+const style = (s) => {
+  if(typeof s === object) {
+    //then for each key write some css
+  }
+
+  else if(typeof s === string) {
+    //then assume this is a file path
+  }
+}
+
+module.exports = {style}
